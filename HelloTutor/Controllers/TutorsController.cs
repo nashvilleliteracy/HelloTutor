df2ce43ac97f000c5bd236c5e2112b4b1cffcab0
Michael Mayers <michael.mayers@hcahealthcare.com>
2017-05-09 23:40:40 -0500

    Changed HomeController to redirect to login page. Changed _Layout.cshtml to add Admin dropdown menu. Added Tutors controller Added Tutors View Folder

---
+++ HelloTutor/Controllers/TutorsController.cs
@@ -0,0 +1,32 @@
+﻿using HelloTutor;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloTutor.Controllers
+{
+  public class TutorsController : Controller
+  {
+    // GET: Tutors
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    public ActionResult Tutors_Read([DataSourceRequest]DataSourceRequest request)
+    {
+      using (var HiTutor = new HelloTutorEntities())
+      {
+        IQueryable<Tutor> tutors = HiTutor.Tutors;
+        DataSourceResult result = tutors.ToDataSourceResult(request);
+        return Json(result);
+      }
+
+    }
+  }
+}
